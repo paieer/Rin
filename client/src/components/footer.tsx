72e6f2311d152997613a4fa3281b9224a63344fd
paieer <paieer@users.noreply.github.com>
2025-03-03 09:19:47 +0800

    fix: 更新Footer

--- client/src/components/footer.tsx
+++ client/src/components/footer.tsx
@@ -59,7 +59,7 @@ function Footer() {
                             setDoubleClickTimes(doubleClickTimes + 1)
                         }
                     }}>
-                        © 2024 Powered by <a className='hover:underline' href="https://github.com/openRin/Rin" target="_blank">Rin</a>
+                        © 2024 Powered by OpenCompanyReport
                     </span>
                     {config.get<boolean>('rss') && <>
                         <Spliter />
