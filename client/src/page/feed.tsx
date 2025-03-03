ccca890b31a7082d7168fd9a59da5b3f4570cd56
paieer <paieer@users.noreply.github.com>
2025-03-03 09:16:39 +0800

    refactor: 移除FeedPage中的用户头像和用户名显示

--- client/src/page/feed.tsx
+++ client/src/page/feed.tsx
@@ -286,17 +286,6 @@ export function FeedPage({ id, TOC, clean }: { id: string, TOC: () => JSX.Elemen
                       ))}
                     </div>
                   )}
-                  <div className="flex flex-row items-center">
-                    <img
-                      src={feed.user.avatar || "/avatar.png"}
-                      className="w-8 h-8 rounded-full"
-                    />
-                    <div className="ml-2">
-                      <span className="text-gray-400 text-sm cursor-default">
-                        {feed.user.username}
-                      </span>
-                    </div>
-                  </div>
                 </div>
               </article>
               <AdjacentSection id={id} setError={setError}/>
