3140783599c49652892148a6249515d32715c7c2
paieer <paieer@users.noreply.github.com>
2025-03-05 03:23:42 +0800

    feat: 在feeds表中添加top字段并设置为NOT NULL

--- server/src/db/schema.ts
+++ server/src/db/schema.ts
@@ -1,112 +1,130 @@
 import { relations, sql } from "drizzle-orm";
 import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
-const created_at = integer("created_at", { mode: 'timestamp' }).default(sql`(unixepoch())`).notNull();
-const updated_at = integer("updated_at", { mode: 'timestamp' }).default(sql`(unixepoch())`).notNull();
+const created_at = integer("created_at", { mode: "timestamp" })
+  .default(sql`(unixepoch())`)
+  .notNull();
+const updated_at = integer("updated_at", { mode: "timestamp" })
+  .default(sql`(unixepoch())`)
+  .notNull();
 
 export const feeds = sqliteTable("feeds", {
-    id: integer("id").primaryKey(),
-    alias: text("alias"),
-    title: text("title"),
-    summary: text("summary").default("").notNull(),
-    content: text("content").notNull(),
-    listed: integer("listed").default(1).notNull(),
-    draft: integer("draft").default(1).notNull(),
-    top: integer("top").default(0).notNull(),
-    uid: integer("uid").references(() => users.id).notNull(),
-    createdAt: created_at,
-    updatedAt: updated_at,
+  id: integer("id").primaryKey(),
+  alias: text("alias").unique(),
+  title: text("title"),
+  summary: text("summary").default("").notNull(),
+  content: text("content").notNull(),
+  listed: integer("listed").default(1).notNull(),
+  draft: integer("draft").default(1).notNull(),
+  top: integer("top").default(0).notNull(),
+  uid: integer("uid")
+    .references(() => users.id)
+    .notNull(),
+  createdAt: created_at,
+  updatedAt: updated_at,
 });
 
 export const visits = sqliteTable("visits", {
-    id: integer("id").primaryKey(),
-    feedId: integer("feed_id").references(() => feeds.id, { onDelete: 'cascade' }).notNull(),
-    ip: text("ip").notNull(),
-    createdAt: created_at,
+  id: integer("id").primaryKey(),
+  feedId: integer("feed_id")
+    .references(() => feeds.id, { onDelete: "cascade" })
+    .notNull(),
+  ip: text("ip").notNull(),
+  createdAt: created_at,
 });
 
 export const info = sqliteTable("info", {
-    key: text("key").notNull().unique(),
-    value: text("value").notNull(),
+  key: text("key").notNull().unique(),
+  value: text("value").notNull(),
 });
 
 export const friends = sqliteTable("friends", {
-    id: integer("id").primaryKey(),
-    name: text("name").notNull(),
-    desc: text("desc"),
-    avatar: text("avatar").notNull(),
-    url: text("url").notNull(),
-    uid: integer("uid").references(() => users.id, { onDelete: 'cascade' }).notNull(),
-    accepted: integer("accepted").default(0).notNull(),
-    health: text("health").default("").notNull(),
-    createdAt: created_at,
-    updatedAt: updated_at,
+  id: integer("id").primaryKey(),
+  name: text("name").notNull(),
+  desc: text("desc"),
+  avatar: text("avatar").notNull(),
+  url: text("url").notNull(),
+  uid: integer("uid")
+    .references(() => users.id, { onDelete: "cascade" })
+    .notNull(),
+  accepted: integer("accepted").default(0).notNull(),
+  health: text("health").default("").notNull(),
+  createdAt: created_at,
+  updatedAt: updated_at,
 });
 
 export const users = sqliteTable("users", {
-    id: integer("id").primaryKey(),
-    username: text("username").notNull(),
-    openid: text("openid").notNull(),
-    avatar: text("avatar"),
-    permission: integer("permission").default(0),
-    createdAt: created_at,
-    updatedAt: updated_at,
+  id: integer("id").primaryKey(),
+  username: text("username").notNull(),
+  openid: text("openid").notNull(),
+  avatar: text("avatar"),
+  permission: integer("permission").default(0),
+  createdAt: created_at,
+  updatedAt: updated_at,
 });
 
 export const comments = sqliteTable("comments", {
-    id: integer("id").primaryKey(),
-    feedId: integer("feed_id").references(() => feeds.id, { onDelete: 'cascade' }).notNull(),
-    userId: integer("user_id").references(() => users.id, { onDelete: 'cascade' }).notNull(),
-    content: text("content").notNull(),
-    createdAt: created_at,
-    updatedAt: updated_at,
+  id: integer("id").primaryKey(),
+  feedId: integer("feed_id")
+    .references(() => feeds.id, { onDelete: "cascade" })
+    .notNull(),
+  userId: integer("user_id")
+    .references(() => users.id, { onDelete: "cascade" })
+    .notNull(),
+  content: text("content").notNull(),
+  createdAt: created_at,
+  updatedAt: updated_at,
 });
 
 export const hashtags = sqliteTable("hashtags", {
-    id: integer("id").primaryKey(),
-    name: text("name").notNull(),
-    createdAt: created_at,
-    updatedAt: updated_at,
+  id: integer("id").primaryKey(),
+  name: text("name").notNull(),
+  createdAt: created_at,
+  updatedAt: updated_at,
 });
 
 export const feedHashtags = sqliteTable("feed_hashtags", {
-    feedId: integer("feed_id").references(() => feeds.id, { onDelete: 'cascade' }).notNull(),
-    hashtagId: integer("hashtag_id").references(() => hashtags.id, { onDelete: 'cascade' }).notNull(),
-    createdAt: created_at,
-    updatedAt: updated_at,
+  feedId: integer("feed_id")
+    .references(() => feeds.id, { onDelete: "cascade" })
+    .notNull(),
+  hashtagId: integer("hashtag_id")
+    .references(() => hashtags.id, { onDelete: "cascade" })
+    .notNull(),
+  createdAt: created_at,
+  updatedAt: updated_at,
 });
 
 export const feedsRelations = relations(feeds, ({ many, one }) => ({
-    hashtags: many(feedHashtags),
-    user: one(users, {
-        fields: [feeds.uid],
-        references: [users.id],
-    }),
-    comments: many(comments),
+  hashtags: many(feedHashtags),
+  user: one(users, {
+    fields: [feeds.uid],
+    references: [users.id],
+  }),
+  comments: many(comments),
 }));
 
 export const commentsRelations = relations(comments, ({ one }) => ({
-    feed: one(feeds, {
-        fields: [comments.feedId],
-        references: [feeds.id],
-    }),
-    user: one(users, {
-        fields: [comments.userId],
-        references: [users.id],
-    }),
+  feed: one(feeds, {
+    fields: [comments.feedId],
+    references: [feeds.id],
+  }),
+  user: one(users, {
+    fields: [comments.userId],
+    references: [users.id],
+  }),
 }));
 
 export const hashtagsRelations = relations(hashtags, ({ many }) => ({
-    feeds: many(feedHashtags),
+  feeds: many(feedHashtags),
 }));
 
 export const feedHashtagsRelations = relations(feedHashtags, ({ one }) => ({
-    feed: one(feeds, {
-        fields: [feedHashtags.feedId],
-        references: [feeds.id],
-    }),
-    hashtag: one(hashtags, {
-        fields: [feedHashtags.hashtagId],
-        references: [hashtags.id],
-    }),
+  feed: one(feeds, {
+    fields: [feedHashtags.feedId],
+    references: [feeds.id],
+  }),
+  hashtag: one(hashtags, {
+    fields: [feedHashtags.hashtagId],
+    references: [hashtags.id],
+  }),
 }));
