ad618473ab15843666c407246692268a91ea256a
paieer <paieer@users.noreply.github.com>
2025-03-10 14:10:16 +0800

    调整FeedService

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,33 +1,35 @@
-import cors from '@elysiajs/cors';
-import { serverTiming } from '@elysiajs/server-timing';
-import { Elysia } from 'elysia';
-import { CommentService } from './services/comments';
+import cors from "@elysiajs/cors";
+import { serverTiming } from "@elysiajs/server-timing";
+import { Elysia } from "elysia";
+import { CommentService } from "./services/comments";
 import { FaviconService } from "./services/favicon";
-import { FeedService } from './services/feed';
-import { FriendService } from './services/friends';
-import { RSSService } from './services/rss';
-import { SEOService } from './services/seo';
-import { StorageService } from './services/storage';
-import { TagService } from './services/tag';
-import { UserService } from './services/user';
-import { ConfigService } from './services/config';
+import { FeedService } from "./services/feed";
+import { FriendService } from "./services/friends";
+import { RSSService } from "./services/rss";
+import { SEOService } from "./services/seo";
+import { StorageService } from "./services/storage";
+import { TagService } from "./services/tag";
+import { UserService } from "./services/user";
+import { ConfigService } from "./services/config";
 
-export const app = () => new Elysia({ aot: false })
-    .use(cors({
+export const app = () =>
+  new Elysia({ aot: false })
+    .use(
+      cors({
         aot: false,
-        origin: '*',
-        methods: '*',
-        allowedHeaders: [
-            'authorization',
-            'content-type'
-        ],
+        origin: "*",
+        methods: "*",
+        allowedHeaders: ["authorization", "content-type"],
         maxAge: 600,
         credentials: true,
-        preflight: true
-    }))
-    .use(serverTiming({
-        enabled: true,
-    }))
+        preflight: true,
+      })
+    )
+    .use(
+      serverTiming({
+        enabled: false,
+      })
+    )
     .use(UserService())
     .use(FaviconService())
     .use(FeedService())
@@ -38,10 +40,10 @@ export const app = () => new Elysia({ aot: false })
     .use(SEOService())
     .use(RSSService())
     .use(ConfigService())
-    .get('/', () => `Hi`)
+    .get("/", () => `Hi`)
     .onError(({ path, params, code }) => {
-        if (code === 'NOT_FOUND')
-            return `${path} ${JSON.stringify(params)} not found`
-    })
+      if (code === "NOT_FOUND")
+        return `${path} ${JSON.stringify(params)} not found`;
+    });
 
 export type App = ReturnType<typeof app>;
