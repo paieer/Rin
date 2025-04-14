d5a5b3b94359754de4fb75794441dedb06a602a6
paieer <paieer@users.noreply.github.com>
2025-04-14 09:06:23 +0800

    每周自动清除cache

--- server/src/_worker.ts
+++ server/src/_worker.ts
@@ -1,53 +1,55 @@
-import { drizzle, DrizzleD1Database } from "drizzle-orm/d1";
-import { Elysia } from "elysia";
-import 'reflect-metadata';
-import Container from "typedi";
-import type { Env } from "./db/db";
-import * as schema from './db/schema';
-import { app } from "./server";
-import { friendCrontab } from "./services/friends";
-import { rssCrontab } from "./services/rss";
-import { CacheImpl } from "./utils/cache";
-import { dbToken, envToken } from "./utils/di";
+import { drizzle, DrizzleD1Database } from "drizzle-orm/d1"
+import { Elysia } from "elysia"
+import "reflect-metadata"
+import Container from "typedi"
+import type { Env } from "./db/db"
+import * as schema from "./db/schema"
+import { app } from "./server"
+import { friendCrontab } from "./services/friends"
+import { rssCrontab } from "./services/rss"
+import { CacheImpl } from "./utils/cache"
+import { dbToken, envToken } from "./utils/di"
 export type DB = DrizzleD1Database<typeof import("./db/schema")>
 
 export default {
-    async fetch(
-        request: Request,
-        env: Env,
-    ): Promise<Response> {
+  async fetch(request: Request, env: Env): Promise<Response> {
+    const db = drizzle(env.DB, { schema: schema })
+    Container.set(envToken, env)
+    Container.set(dbToken, db)
+
+    const exist = Container.has("cache")
+    if (!exist) {
+      Container.set("cache", new CacheImpl())
+      Container.set("server.config", new CacheImpl("server.config"))
+      Container.set("client.config", new CacheImpl("client.config"))
+    }
+
+    return await new Elysia({ aot: false }).use(app()).handle(request)
+  },
+  async scheduled(
+    _controller: ScheduledController,
+    env: Env,
+    ctx: ExecutionContext
+  ) {
+    switch (_controller.cron) {
+      case "*/20 * * * *":
         const db = drizzle(env.DB, { schema: schema })
         Container.set(envToken, env)
         Container.set(dbToken, db)
 
         const exist = Container.has("cache")
         if (!exist) {
-            Container.set("cache", new CacheImpl());
-            Container.set("server.config", new CacheImpl("server.config"));
-            Container.set("client.config", new CacheImpl("client.config"));
-        }
-
-        return await new Elysia({ aot: false })
-            .use(app())
-            .handle(request)
-    },
-    async scheduled(
-        _controller: ScheduledController | null,
-        env: Env,
-        ctx: ExecutionContext
-    ) {
-        const db = drizzle(env.DB, { schema: schema })
-        Container.set(envToken, env)
-        Container.set(dbToken, db)
-
-        const exist = Container.has("cache")
-        if (!exist) {
-            Container.set("cache", new CacheImpl());
-            Container.set("server.config", new CacheImpl("server.config"));
-            Container.set("client.config", new CacheImpl("client.config"));
+          Container.set("cache", new CacheImpl())
+          Container.set("server.config", new CacheImpl("server.config"))
+          Container.set("client.config", new CacheImpl("client.config"))
         }
 
         await friendCrontab(env, ctx)
         await rssCrontab(env)
-    },
+        break
+      case "1 0 * * 1":
+        new CacheImpl("cache").clear()
+        break
+    }
+  },
 }
